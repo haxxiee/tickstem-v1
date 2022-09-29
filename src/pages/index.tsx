d00983b7a2ea046f88dd8d04274c5b737d509a97
Linus Foxell <linusfoxell@hotmail.com>
2022-09-29 11:11:38 +0200

    add: dashboard + loading component + minor stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
 import { trpc } from "../utils/trpc";
-// import { signIn, signOut, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
   // const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
@@ -17,26 +18,47 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto flex flex-col items-center justify-center min-h-full p-4">
+      <main className="container mx-auto flex flex-col items-center text-center justify-center p-4 mt-60">
         <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-          Create <span className="text-purple-300">T3</span> App
+          TIC<span className="text-purple-300">KS</span>TEM
         </h1>
-        <p className="text-2xl text-gray-700">This stack uses:</p>
+        <p className="text-2xl text-gray-700">A system for ticket reports!</p>
+        <p className="text-xl text-gray-700">Sign in to report your problem</p>
+
         <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3"></div>
         {/* <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
           {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
         </div> */}
 
-        {tickets?.map((ticket) => {
+        {/* {tickets?.map((ticket) => {
           return (
             <div key={ticket.id}>
               <div>{ticket.description}</div>
             </div>
           );
-        })}
+        })} */}
       </main>
     </>
   );
 };
+
+export async function getServerSideProps(context: any) {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
+
+  if (session?.user) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  } else {
+    return { props: {} };
+  }
+}
 
 export default Home;
